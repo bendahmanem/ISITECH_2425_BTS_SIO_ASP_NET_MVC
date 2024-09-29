a9745f66f5b7fbc869b7befcdd02917f57da1afc
mnbd <mnbdpro@gmail.com>
2024-09-29 19:09:29 +0200

    feat: data seeding

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,4 +11,52 @@ public class ApplicationDbContext : DbContext
     // Elle est responsable du mapping objet-relationnel
     public DbSet<Student> Roster { get; set; }
     public DbSet<Instructor> Instructors { get; set; }
+
+
+    // Ajout de mock data
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Student>().HasData(
+            new Student()
+            {
+                StudentId = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                AdmissionDate = DateTime.Parse("1990-01-01"),
+                GPA = 3.5,
+                Major = Major.IT,
+                IsVeteran = false
+            });
+        modelBuilder.Entity<Instructor>().HasData(
+            new Instructor()
+            {
+                InstructorId = 1,
+                FirstName = "Jane",
+                LastName = "Doe",
+                IsTenured = true,
+                HiringDate = DateTime.Parse("2010-01-01"),
+                Rank = Ranks.AssociateProfessor
+            },
+            new Instructor()
+            {
+                InstructorId = 2,
+                FirstName = "John",
+                LastName = "Smith",
+                IsTenured = false,
+                HiringDate = DateTime.Parse("2015-01-01"),
+                Rank = Ranks.Instructor
+            },
+            new Instructor()
+            {
+                InstructorId = 3,
+                FirstName = "Jane",
+                LastName = "Smith",
+                IsTenured = true,
+                HiringDate = DateTime.Parse("2012-01-01"),
+                Rank = Ranks.FullProfessor
+            }
+
+            );
+    }
 }
