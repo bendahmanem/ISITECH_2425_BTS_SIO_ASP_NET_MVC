e20a313a553bcc80330c0c4a4023aaab6beef1d5
mnbd <mnbdpro@gmail.com>
2024-09-29 16:27:19 +0200

    feat: tagHelpers and DataAnnotations in Index and ShowDetail

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -1,15 +1,31 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASPBookProject.Models;
 
-public enum Ranks { Adjunct, Instructor, AssistantProfessor, AssociateProfessor, FullProfessor }
+// we can also data annotations to specify the display name of the properties
+// of the enum
+public enum Ranks { Adjunct, Instructor, [Display(Name = "Assistant Professor")] AssistantProfessor, [Display(Name = "Associate Professor")] AssociateProfessor, [Display(Name = "Full Professor")] FullProfessor }
 
 public class Instructor
 {
+
+    [Display(Name = "Instructor ID")]
     public int InstructorId { get; set; }
+
+    [Display(Name = "First Name")]
     public string? FirstName { get; set; }
+
+    [Display(Name = "Last Name")]
     public string? LastName { get; set; }
+
+    [Display(Name = "Is Tenured")]
     public bool IsTenured { get; set; }
+
+    [Display(Name = "Rank")]
     public Ranks Rank { get; set; }
+
+    [Display(Name = "Hiring Date")]
+    [DataType(DataType.Date)]
     public DateTime HiringDate { get; set; }
 }
